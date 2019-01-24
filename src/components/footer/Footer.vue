960f9961c417b7323c3da7e10d9136ba979bb21c
litao12345 <1069295054@qq.com>
2019-01-24 11:30:04 +0800

    底部和猜你喜欢

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,96 @@
+<template>
+	<div class="footer">
+		<ul>
+			<li
+				v-for="(list, index) in classType"
+				@click="gotoAddress(list.path, index)"
+				:class="{ show: list.classShow }"
+				:key="list.id"
+			>
+				<i :class="list.className"></i>
+				<div>{{ list.typeName }}</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Footer',
+	data() {
+		return {
+			classType: [
+				{
+					path: { path: '/Test' },
+					classShow: true,
+					className: 'el-icon-phone-shouye2',
+					typeName: '首页'
+				},
+				{
+					path: { path: '/Like' },
+					classShow: false,
+					className: 'el-icon-phone-sousuoremenfenlei',
+					typeName: '分类'
+				},
+				{
+					path: { path: '/Test' },
+					classShow: false,
+					className: 'el-icon-phone-faxian2',
+					typeName: '发现'
+				},
+				{
+					path: { path: '/Test' },
+					classShow: false,
+					className: 'el-icon-phone-gouwuche1',
+					typeName: '购物车'
+				},
+				{
+					path: { path: '/Test' },
+					classShow: false,
+					className: 'el-icon-phone-wode2',
+					typeName: '我的'
+				}
+			],
+			className: [
+				{ show: 'el-icon-phone-shouye', hide: 'el-icon-phone-shouye2' },
+				{ show: 'el-icon-phone-sousuoremenfenleie', hide: 'el-icon-phone-fenlei1' },
+				{ show: 'el-icon-phone-faxian2', hide: 'el-icon-phone-faxian1' },
+				{ show: 'el-icon-phone-gouwuche1', hide: 'el-icon-phone-ziyuan' },
+				{ show: 'el-icon-phone-wode2', hide: 'el-icon-phone-wode1' }
+			]
+		};
+	},
+	methods: {
+		gotoAddress(path, index) {
+			this.$router.push(path);
+			this.classType.map(item => {
+				item.classShow = false;
+			});
+			this.classType[index].classShow = true;
+			this.classType[index].className = this.className[index].hide;
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+	border-top: 1px solid #eee;
+	padding: 5px 0;
+	width: 100%;
+	position: fixed;
+	bottom: 0;
+	ul {
+		width: 100%;
+		li {
+			width: 20%;
+			text-align: center;
+			float: left;
+			cursor: pointer;
+		}
+	}
+}
+.show {
+	color: blue;
+}
+</style>
---
+++ src/page/like/Like.vue
@@ -0,0 +1,115 @@
+<template>
+	<div class="hello">
+		<div class="swiper-container">
+			<h2>猜你喜欢</h2>
+			<div class="swiper-wrapper">
+				<div class="swiper-slide">
+					<img src="../../assets/1.jpg">
+					<div>
+						<p class="name">魅蓝3</p>
+						<p class="price">￥566</p>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="../../assets/1.jpg">
+					<div>
+						<p class="name">魅蓝5</p>
+						<p class="price">￥586</p>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="../../assets/1.jpg">
+					<div>
+						<p class="name">魅蓝3</p>
+						<p class="price">￥566</p>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="../../assets/1.jpg">
+					<div>
+						<p class="name">魅蓝3</p>
+						<p class="price">￥566</p>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="../../assets/1.jpg">
+					<div>
+						<p class="name">魅蓝3</p>
+						<p class="price">￥566</p>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="../../assets/1.jpg">
+					<div>
+						<p class="name">魅蓝3</p>
+						<p class="price">￥566</p>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="../../assets/1.jpg">
+					<div>
+						<p class="name">魅蓝3</p>
+						<p class="price">￥566</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import 'swiper-css';
+import Swiper from 'swiper-js';
+export default {
+	name: 'Like',
+	data() {
+		return {
+			msg: '发现'
+		};
+	},
+	mounted() {
+		var mySwiper = new Swiper('.swiper-container', {
+			freeMode:false,
+			slidesPerView: 3,
+		});
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.swiper-container {
+	width: 100%;
+	background-color: rgb(244,244,244);
+	h2{
+		text-align: center;
+		&:after{
+			content: '\2013';
+			display: block;
+			color:blue;
+			height: 15px;
+			line-height: 15px;
+		}
+	}
+	.swiper-slide {
+		margin-left: 5px;
+		width: 100%;
+		div{
+			background-color: white;
+			padding-left: 5px;
+		}
+		img{
+			width: 100%;
+			height: 80%
+		}
+		.price{
+			color:red;
+			font-size: 1.1rem;
+			font-weight: bolder;
+		}
+		.name{
+			font-size: 1.3rem;
+			margin-bottom: 15px;
+		}
+	}
+}
+</style>
